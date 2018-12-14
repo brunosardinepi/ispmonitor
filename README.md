b91aea4158764fae032d55e6c5f16ff30e90be5d
brunosardinepi <gn9012@gmail.com>
2018-12-14 23:01:20 +0000

    consolidate purge scripts, change saved results to 7 day retention

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Installation requirements
 
-*/5 * * * * /home/me/ispmonitor/ispmonitorenv/bin/python3 /home/me/ispmonitor/manage.py run_ping
-*/5 * * * * /home/me/ispmonitor/ispmonitorenv/bin/python3 /home/me/ispmonitor/manage.py delete_results_gte_24_hours
-0 1 * * * /home/me/ispmonitor/ispmonitorenv/bin/python3 /home/me/ispmonitor/manage.py delete_stale_monitors
+*/5 * * * * /home/user/ispmonitor/ispmonitorenv/bin/python3 /home/user/ispmonitor/manage.py run_ping
+*/5 * * * * /home/user/ispmonitor/ispmonitorenv/bin/python3 /home/user/ispmonitor/manage.py delete_results_gte_24_hours
+0 1 * * * /home/user/ispmonitor/ispmonitorenv/bin/python3 /home/user/ispmonitor/manage.py purge_seven_days
 
--- monitors/management/commands/purge_seven_days.py
+++ monitors/management/commands/purge_seven_days.py
@@ -1,15 +1,17 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from monitors.models import Monitor
+from monitors.models import Monitor, Result
 
 
 class Command(BaseCommand):
-    help = "Deletes monitors that haven't been viewed in 7 days"
-
     def handle(self, *args, **options):
         # define time range as 7 days
         time_range = timezone.now() - timezone.timedelta(days=7)
 
         # delete monitors that haven't been viewed in 7 days
         Monitor.objects.filter(last_viewed__lt=time_range).delete()
+
+        # delete results older than 7 days that are saved
+        Result.objects.filter(is_saved=True, date_created__lt=time_range).delete()
+
