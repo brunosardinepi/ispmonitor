1f0250beda234582554572c04ebeccf0e46170c8
brunosardinepi <gn9012@gmail.com>
2018-12-18 22:11:15 +0000

    account for abnormal response

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ pip install -r requirements.txt
 
 */5 * * * * /home/user/ispmonitor/ispmonitorenv/bin/python3 /home/user/ispmonitor/manage.py get_result
 
-*/5 * * * * /home/user/ispmonitor/ispmonitorenv/bin/python3 /home/user/ispmonitor/manage.py delete_results_gte_24_hours
+0 * * * * /home/user/ispmonitor/ispmonitorenv/bin/python3 /home/user/ispmonitor/manage.py delete_results_gte_24_hours
 
 0 1 * * * /home/user/ispmonitor/ispmonitorenv/bin/python3 /home/user/ispmonitor/manage.py purge_seven_days
 ```
--- monitors/management/commands/get_result.py
+++ monitors/management/commands/get_result.py
@@ -42,25 +42,32 @@ class Command(BaseCommand):
         lines = content.splitlines()
 
         # find the line that has "packet loss"
-        packet_loss = [x for x in lines if "packet loss" in x][0]
-
-        # isolate the packet loss number
-        packet_loss = packet_loss.split(",")[2]
-        packet_loss = packet_loss.split("%")[0]
-        packet_loss = packet_loss.strip()
-        packet_loss = int(packet_loss)
+        packet_loss = [x for x in lines if "packet loss" in x]
+        if packet_loss:
+            # isolate the packet loss number
+            packet_loss = packet_loss[0]
+            packet_loss = packet_loss.split(",")[2]
+            packet_loss = packet_loss.split("%")[0]
+            packet_loss = packet_loss.strip()
+            packet_loss = int(packet_loss)
+        else:
+            # couldn't find "packet loss" in output, so must not be a normal response
+            packet_loss = 100
 
         # find the line that has "min/avg/max/mdev"
-        latency = [x for x in lines if "min/avg/max/mdev" in x][0]
-
-        # isolate the "avg" latency
-        latency = latency.split("/")[4]
-        latency = latency.strip()
-        latency = Decimal(latency)
-
+        latency = [x for x in lines if "min/avg/max/mdev" in x]
+        if latency:
+            # isolate the "avg" latency
+            latency = latency[0]
+            latency = latency.split("/")[4]
+            latency = latency.strip()
+            latency = Decimal(latency)
+        else:
+            # couldn't find "latency" in output, so must not be a normal response
+            latency = Decimal(9999)
 
         # create a result for this monitor
-        return Result.objects.create(
+        result = Result.objects.create(
                    monitor=monitor,
                    content=content,
                    packet_loss=packet_loss,
@@ -72,10 +79,13 @@ class Command(BaseCommand):
             result.is_saved = True
             result.save()
 
+        # return the result
+        return result
+
     def handle(self, *args, **options):
         # get all of the monitors
         monitors = Monitor.objects.all()
 
         if monitors:
             for monitor in monitors:
-                result = self.create_result(monitor)
+                self.create_result(monitor)
